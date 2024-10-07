449b8f59dc8fba52757ff542a9f060a1fdd9ef7f
logan114 <kepirobalazs204@gmail.com>
2024-10-07 09:47:34 +0200

    beadás, feladás

---
+++ src/controller/ControllerKonzol.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+import model.Ember;
+/**
+ *
+ * @author kepiro.balazs
+ */
+public class ControllerKonzol {
+    private String emberAdatok;
+
+    public ControllerKonzol(String emberAdatok) {
+        this.emberAdatok = emberAdatok;
+
+    }
+    
+   
+}
